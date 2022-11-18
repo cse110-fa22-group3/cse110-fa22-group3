b654d480110973f7bdb654d9c8dc5f4320825d86
cepeters-ucsd <cepeters@ucsd.edu>
2022-11-17 16:49:22 -0800

    Fixed chore editing

--- src/front-end/modules/chores-list/scripts/main.js
+++ src/front-end/modules/chores-list/scripts/main.js
@@ -26,9 +26,9 @@ function initPage(chores) {
 
   let choreBoxes = document.querySelectorAll("chore-card");
   let edit_div = document.querySelector("#edit-background");
-  let edit_chore_name = document.querySelector("form#edit input#c-choreName");
+  let edit_chore_name = document.querySelector("form#edit input#e-choreName");
   let edit_roommate_name = document.querySelector(
-    "form#edit input#roommateName"
+    "form#edit input#e-roommateName"
   );
 
   choreBoxes.forEach((card) => {
