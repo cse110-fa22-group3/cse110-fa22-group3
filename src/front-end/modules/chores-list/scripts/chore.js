4c5d4d25039622ca281d4e100f825e95e9a36a44
cepeters-ucsd <cepeters@ucsd.edu>
2022-11-23 00:18:32 -0800

    Made standard changes

--- src/front-end/modules/chores-list/scripts/chore.js
+++ src/front-end/modules/chores-list/scripts/chore.js
@@ -1,25 +1,25 @@
 class chore extends HTMLElement {
-  constructor() {
-    super();
-    this.shadow = this.attachShadow({ mode: "open" });
-    const chore = document.createElement("div");
-    chore.className = "float-child edit-chore";
-    this.shadow.append(chore);
+  constructor () {
+    super()
+    this.shadow = this.attachShadow({ mode: 'open' })
+    const chore = document.createElement('div')
+    chore.className = 'float-child edit-chore'
+    this.shadow.append(chore)
   }
 
-  set data(data) {
-    if (!data) return;
-    const chore = this.shadow.querySelector("div");
+  set data (data) {
+    if (!data) return
+    const chore = this.shadow.querySelector('div')
     chore.innerHTML = `<p>
         <h2>${data.choreName}</h2>
         <h3>${data.roommateName}</h3>
     </p>
-        `;
+        `
   }
 
-  get data() {
-    const chore = this.shadow.querySelector("div");
-    return chore.innerHTML;
+  get data () {
+    const chore = this.shadow.querySelector('div')
+    return chore.innerHTML
   }
 }
-customElements.define("chore-card", chore);
+customElements.define('chore-card', chore)
