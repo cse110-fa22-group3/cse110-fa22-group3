e6bab2fbe323621bdbdcb0e07abd01c67a9beecd
Atomgroup530 <83523169+Atomgroup530@users.noreply.github.com>
2022-11-28 12:22:46 -0800

    Create frontend.test.js

---
+++ _tests_/frontend.test.js
@@ -0,0 +1,4 @@
+// frontend.test.js
+
+const mainFunctions = require('./src/front-end/modules/roommate-list/js/main.js');
+const roommateFunctions = require('./src/front-end/modules/roommate-list/js/roommate.js');
