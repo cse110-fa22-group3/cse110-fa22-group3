5d86ebdca4dca9107d02c254f5d7bd183951373b
Kalyan-Sunkara <Kalyan-Sunkara@users.noreply.github.com>
2022-11-29 10:13:31 +0000

    Prettified Code!

--- src/_tests_/passwordHolderTests/unit.test.js
+++ src/_tests_/passwordHolderTests/unit.test.js
@@ -1,21 +1,23 @@
 // unit.test.js
-const functionAPIs = require('../../testSupportFiles/passwordHolderTests/unit-test-passwordHolderAPIs');
+const functionAPIs = require("../../testSupportFiles/passwordHolderTests/unit-test-passwordHolderAPIs");
 
-test('checks if readPasswords returns empty array when localStorage is empty', () => {
-    let array = functionAPIs.readPasswords();
-    console.log(window.localStorage.getItem("PasswordHolderData"));
-    expect(array).toStrictEqual([]);
+test("checks if readPasswords returns empty array when localStorage is empty", () => {
+  let array = functionAPIs.readPasswords();
+  console.log(window.localStorage.getItem("PasswordHolderData"));
+  expect(array).toStrictEqual([]);
 });
 
-test('checks if createPassword works when adding one element', () => {
-    const formData = {
-        key : "Hulu",
-        username: "Kalyan",
-        password: "topSecret1010"
-    }
-    functionAPIs.createPassword(formData);
-    formData.id = 0
+test("checks if createPassword works when adding one element", () => {
+  const formData = {
+    key: "Hulu",
+    username: "Kalyan",
+    password: "topSecret1010",
+  };
+  functionAPIs.createPassword(formData);
+  formData.id = 0;
 
-    const localStorageData = JSON.parse(window.localStorage.getItem("PasswordHolderData"));
-    expect(localStorageData.Passwords[0]).toStrictEqual(formData);
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("PasswordHolderData")
+  );
+  expect(localStorageData.Passwords[0]).toStrictEqual(formData);
 });
