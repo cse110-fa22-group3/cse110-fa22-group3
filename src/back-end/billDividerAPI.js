28e96ce263c1c46672bfa971ae55c385eaf5e876
IzzaTommy <42631988+IzzaTommy@users.noreply.github.com>
2022-11-29 23:17:05 -0800

    added function headers

---
+++ src/back-end/billDividerAPI.js
@@ -0,0 +1,27 @@
+export function getContribution(id)
+{
+
+}
+
+export function getDebt(id) 
+{
+    return getAvgContribution() - getContribution(id);
+}
+
+export function addPayment(text, giver, recipient, amount) 
+{
+}
+
+export function getLog()
+{
+
+}
+
+export function getTotalContributions()
+{
+
+}
+
+export function getAvgContribution() 
+{
+}
