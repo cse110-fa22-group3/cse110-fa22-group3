bd649c17056919ffa3436cc0ded3d0a868cb1a66
Atomgroup530 <xul007@ucsd.edu>
2022-12-02 00:07:38 -0800

    adding multiple roommates failed

--- src/_tests_/roommateListTests/unit.test.js
+++ src/_tests_/roommateListTests/unit.test.js
@@ -1,38 +1,66 @@
-// unit.test.js
+/**
+ * @jest-environment jsdom
+ */
+
 const functionAPIs = require("../../testSupportFiles/roommateListTests/unit-test-roommateListAPI");
 
 test("checks if createRoommate adds a roommate to local storage", () => {
   const formData = {
-    id: 1, 
     name: "Xun Liu",
     birthday: "12/19/2000",
     hobbies: "playing guitar",
     notes: ""
   };
   functionAPIs.createRoommate(formData);
-  formData.id = 0;
   const localStorageData = JSON.parse(
     window.localStorage.getItem("RoommateListData")
   );
-  console.log(window.localStorage.getItem("RoommateListData"));
+  console.log("local storage:", localStorageData.Roommates[0]);
+  console.log("form data:", formData);
   expect(localStorageData.Roommates[0]).toStrictEqual(formData);
   window.localStorage.clear();
 });
 
 test("checks if createRoommate adds an empty roommate to local storage", () => {
   const formData = {
-    id: 1, 
     name: "",
     birthday: "",
     hobbies: "",
     notes: ""
   };
   functionAPIs.createRoommate(formData);
-  formData.id = 0;
   const localStorageData = JSON.parse(
     window.localStorage.getItem("RoommateListData")
   );
-  console.log(window.localStorage.getItem("RoommateListData"));
+  console.log("local storage:", localStorageData.Roommates[0]);
+  console.log("form data:", formData);
   expect(localStorageData.Roommates[0]).toStrictEqual(formData);
+  window.localStorage.clear();
+});
+
+test("checks if createRoommate adds multiple roommates to local storage", () => {
+  const formData1 = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  const formData2 = {
+    name: "John Doe",
+    birthday: "12/12/1212",
+    hobbies: "playing bass",
+    notes: ""
+  };
+  functionAPIs.createRoommate(formData1);
+  functionAPIs.createRoommate(formData2);
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("RoommateListData")
+  );
+  console.log("local storage:", localStorageData.Roommates[0]);
+  console.log("form data:", formData1);
+  console.log("local storage:", localStorageData.Roommates[1]);
+  console.log("form data:", formData2);
+  expect(localStorageData.Roommates[0]).toStrictEqual(formData1);
+  expect(localStorageData.Roommates[1]).toStrictEqual(formData2);
   window.localStorage.clear();
 });
