61502a89c3322e95f37c5105c049a0ba9dff20eb
Atomgroup530 <xul007@ucsd.edu>
2022-12-02 21:58:51 -0800

    updateRoommate test renewed, deleteRoommate test success

--- src/_tests_/roommateListTests/unit.test.js
+++ src/_tests_/roommateListTests/unit.test.js
@@ -2,7 +2,10 @@
  * @jest-environment jsdom
  */
 
+// unit tests for roommateListAPI
+// createRoommate
 const functionAPIs = require("../../testSupportFiles/roommateListTests/unit-test-roommateListAPI");
+const choreFunctionAPIs = require("../../testSupportFiles/choresListTests/unit-test-choresListAPI");
 
 test("checks if createRoommate adds a roommate to local storage", () => {
   const formData = {
@@ -67,6 +70,7 @@ test("checks if createRoommate adds multiple roommates to local storage", () => 
   window.localStorage.clear();
 });
 
+// updateRoommate
 test("checks if updateRoommate updates the properties of a roommate", () => {
   const formData0 = {
     name: "Xun Liu",
@@ -95,7 +99,7 @@ test("checks if updateRoommate updates the properties of a roommate", () => {
   window.localStorage.clear();
 });
 
-test("checks if updateRoommate updates the name of the correct roommate", () => {
+test("checks if updateRoommate updates the properties of the correct roommate", () => {
   const formData0 = {
     name: "Xun Liu",
     birthday: "12/19/2000",
@@ -142,6 +146,7 @@ test("checks if updateRoommate updates the name of the correct roommate", () => 
   window.localStorage.clear();
 });
 
+// readRoommate
 test("checks if readRoommate returns empty array if there's no roommate", () => {
   const Reading = functionAPIs.readRoommate();
   const localStorageData = JSON.parse(
@@ -163,7 +168,7 @@ test("checks if readRoommate returns a roommate array if there's one roommate", 
     notes: ""
   };
   functionAPIs.createRoommate(formData);
-  
+
   const Reading = functionAPIs.readRoommate();
   const localStorageData = JSON.parse(
     window.localStorage.getItem("RoommateListData")
@@ -173,5 +178,110 @@ test("checks if readRoommate returns a roommate array if there's one roommate", 
   console.log("local storage:", Roommates);
   console.log("Reading:", Reading);
   expect(Roommates).toStrictEqual(Reading);
+  window.localStorage.clear();
+});
+
+test("checks if readRoommate returns a roommate array if there're multiple roommates", () => {
+  const formData0 = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  const formData1 = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  const formData2 = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  functionAPIs.createRoommate(formData0);
+  functionAPIs.createRoommate(formData1);
+  functionAPIs.createRoommate(formData2);
+  formData0.id = 0; formData1.id = 1; formData2.id = 2;
+
+  const Reading = functionAPIs.readRoommate();
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("RoommateListData")
+  );
+  const Roommates = localStorageData.Roommates;
+
+  console.log("local storage:", Roommates);
+  console.log("Reading:", Reading);
+  expect(Roommates).toStrictEqual(Reading);
+  window.localStorage.clear();
+});
+
+
+// readRoommate
+test("checks if deleteRoommate deletes a roommate from local storage", () => {
+  choreFunctionAPIs.readChores();
+  const formData = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  const id = 0;
+  const upData = [];
+  functionAPIs.createRoommate(formData);
+  functionAPIs.deleteRoommate(id);
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("RoommateListData")
+  );
+  const Roommates = localStorageData.Roommates;
+  console.log("local storage:", Roommates);
+  console.log("form data:", upData);
+  expect(localStorageData.Roommates).toStrictEqual(upData);
+  window.localStorage.clear();
+});
+
+test("checks if deleteRoommate deletes multiple roommates from local storage", () => {
+  choreFunctionAPIs.readChores();
+  const formData0 = {
+    name: "Xun Liu",
+    birthday: "12/19/2000",
+    hobbies: "playing guitar",
+    notes: ""
+  };
+  const formData1 = {
+    name: "Jack Ma",
+    birthday: "11/11/1111",
+    hobbies: "Making money",
+    notes: "I have no interests in money"
+  };
+  const formData2 = {
+    name: "Ellon Ma",
+    birthday: "xx/xx/xxxx",
+    hobbies: "Speaking English",
+    notes: "Hello, I'm Ellon Ma"
+  };
+
+  const upData = [];
+
+  functionAPIs.createRoommate(formData0);
+  functionAPIs.createRoommate(formData1);
+  functionAPIs.createRoommate(formData2);
+
+  formData0.id = 0;
+  formData1.id = 1;
+  formData2.id = 2;
+
+  for (let id = 0; id < 3; id++) {
+    functionAPIs.deleteRoommate(id);
+  }
+
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("RoommateListData")
+  );
+  const Roommates = localStorageData.Roommates;
+  console.log("local storage:", Roommates);
+  console.log("form data:", upData);
+  expect(localStorageData.Roommates).toStrictEqual(upData);
   window.localStorage.clear();
 });
