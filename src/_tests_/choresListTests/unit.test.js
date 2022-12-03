059282d66f0fb88025d571c357a988583a8dbf65
Atomgroup530 <Atomgroup530@users.noreply.github.com>
2022-12-03 21:49:10 +0000

    Prettified Code!

--- src/_tests_/choresListTests/unit.test.js
+++ src/_tests_/choresListTests/unit.test.js
@@ -7,31 +7,31 @@ const functionAPIs = require("../../testSupportFiles/choresListTests/unit-test-c
 const roommateFunctionAPIs = require("../../testSupportFiles/roommateListTests/unit-test-roommateListAPI");
 
 test("checks if readChores initializes fileds correctly if there's no chore", () => {
-    const resData = {
-      chores: [],
-      archived: [],
-      openChoresCount: 0,
-      closedChoresCount: 0,
-      choresCountId: 1,
-    };
-    functionAPIs.readChores();
-    const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
-    console.log("local storage:", localStorageData);
-    console.log("supposed data:", resData);
-    expect(localStorageData).toStrictEqual(resData);
-    window.localStorage.clear();
-  });
+  const resData = {
+    chores: [],
+    archived: [],
+    openChoresCount: 0,
+    closedChoresCount: 0,
+    choresCountId: 1,
+  };
+  functionAPIs.readChores();
+  const localStorageData = JSON.parse(
+    window.localStorage.getItem("ChoresListData")
+  );
+  console.log("local storage:", localStorageData);
+  console.log("supposed data:", resData);
+  expect(localStorageData).toStrictEqual(resData);
+  window.localStorage.clear();
+});
 
 test("checks if an empty chore is created in local storage", () => {
   functionAPIs.readChores();
   const formData = {
-      title: "",
-      description: "",
-      assignee: "",
-      assignedDate: "",
-  }
+    title: "",
+    description: "",
+    assignee: "",
+    assignedDate: "",
+  };
 
   const resData = {
     id: 1,
@@ -40,12 +40,12 @@ test("checks if an empty chore is created in local storage", () => {
     assignee: "",
     assignedDate: "",
     status: "open",
-  }
+  };
 
   functionAPIs.createChore(formData);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   const chores = localStorageData.chores;
   console.log("local storage:", chores[0]);
   console.log("supposed data:", resData);
@@ -56,21 +56,23 @@ test("checks if an empty chore is created in local storage", () => {
 test("checks readChore returns the correct JSON when empty chore is created in local storgae", () => {
   functionAPIs.readChores();
   const formData = {
-      title: "",
-      description: "",
-      assignee: "",
-      assignedDate: "",
-  }
+    title: "",
+    description: "",
+    assignee: "",
+    assignedDate: "",
+  };
 
   const resData = {
-    chores: [{
-      id: 1,
-      title: "",
-      description: "",
-      assignee: "",
-      assignedDate: "",
-      status: "open",
-    }],
+    chores: [
+      {
+        id: 1,
+        title: "",
+        description: "",
+        assignee: "",
+        assignedDate: "",
+        status: "open",
+      },
+    ],
     archived: [],
     openChoresCount: 1,
     closedChoresCount: 0,
@@ -79,8 +81,8 @@ test("checks readChore returns the correct JSON when empty chore is created in l
 
   functionAPIs.createChore(formData);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   console.log("local storage:", localStorageData);
   console.log("supposed data:", resData);
   expect(localStorageData).toStrictEqual(resData);
@@ -94,7 +96,7 @@ test("check if createChore creates a non-empty chore", () => {
     description: "Wash dishes with hand",
     assignee: ["Mark", "Park", "Kate"],
     assignedDate: "11/11/1111",
-  }
+  };
   const resData = {
     id: 1,
     title: "Wash Dishses",
@@ -102,13 +104,13 @@ test("check if createChore creates a non-empty chore", () => {
     assignee: ["Mark", "Park", "Kate"],
     assignedDate: "11/11/1111",
     status: "open",
-    currRoommate: "Mark"
-  }
+    currRoommate: "Mark",
+  };
 
   functionAPIs.createChore(formData);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   const chores = localStorageData.chores;
   console.log("local storage:", chores[0]);
   console.log("supposed data:", formData);
@@ -123,18 +125,20 @@ test("checks readChore returns the correct JSON when a non-empty chore is create
     description: "Wash dishes with hand",
     assignee: ["Mark", "Park", "Kate"],
     assignedDate: "11/11/1111",
-  }
+  };
 
   const resData = {
-    chores: [{
-      id: 1,
-      title: "Wash Dishses",
-      description: "Wash dishes with hand",
-      assignee: ["Mark", "Park", "Kate"],
-      assignedDate: "11/11/1111",
-      status: "open",
-      currRoommate: "Mark"
-    }],
+    chores: [
+      {
+        id: 1,
+        title: "Wash Dishses",
+        description: "Wash dishes with hand",
+        assignee: ["Mark", "Park", "Kate"],
+        assignedDate: "11/11/1111",
+        status: "open",
+        currRoommate: "Mark",
+      },
+    ],
     archived: [],
     openChoresCount: 1,
     closedChoresCount: 0,
@@ -143,8 +147,8 @@ test("checks readChore returns the correct JSON when a non-empty chore is create
 
   functionAPIs.createChore(formData);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   console.log("local storage:", localStorageData);
   console.log("supposed data:", resData);
   expect(localStorageData).toStrictEqual(resData);
@@ -173,40 +177,42 @@ test("check if createChore creates multiple non-empty chore", () => {
     assignedDate: "11/19/1111",
   };
 
-  const resData = [{
-    id: 1,
-    title: "Wash Dishses",
-    description: "Wash dishes with hand",
-    assignee: ["Mark", "Park", "Kate"],
-    assignedDate: "11/11/1111",
-    status: "open",
-    currRoommate: "Mark"
-  },
-  {
-    id: 2,
-    title: "Clean floor",
-    description: "Clean floor with vaccum",
-    assignee: ["Park", "Mark", "Kate"],
-    assignedDate: "11/12/1111",
-    status: "open",
-    currRoommate: "Park"
-  },
-  {
-    id: 3,
-    title: "Move TV",
-    description: "Move TV away from there",
-    assignee: ["Kate", "Mark", "Park"],
-    assignedDate: "11/19/1111",
-    status: "open",
-    currRoommate: "Kate"
-  }];
+  const resData = [
+    {
+      id: 1,
+      title: "Wash Dishses",
+      description: "Wash dishes with hand",
+      assignee: ["Mark", "Park", "Kate"],
+      assignedDate: "11/11/1111",
+      status: "open",
+      currRoommate: "Mark",
+    },
+    {
+      id: 2,
+      title: "Clean floor",
+      description: "Clean floor with vaccum",
+      assignee: ["Park", "Mark", "Kate"],
+      assignedDate: "11/12/1111",
+      status: "open",
+      currRoommate: "Park",
+    },
+    {
+      id: 3,
+      title: "Move TV",
+      description: "Move TV away from there",
+      assignee: ["Kate", "Mark", "Park"],
+      assignedDate: "11/19/1111",
+      status: "open",
+      currRoommate: "Kate",
+    },
+  ];
 
   functionAPIs.createChore(formData0);
   functionAPIs.createChore(formData1);
   functionAPIs.createChore(formData2);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   const chores = localStorageData.chores;
   console.log("local storage:", chores);
   console.log("supposed data:", resData);
@@ -237,33 +243,35 @@ test("checks readChore returns the correct JSON when multiple non-empty chores a
   };
 
   const resData = {
-    chores: [{
-      id: 1,
-      title: "Wash Dishses",
-      description: "Wash dishes with hand",
-      assignee: ["Mark", "Park", "Kate"],
-      assignedDate: "11/11/1111",
-      status: "open",
-      currRoommate: "Mark"
-    },
-    {
-      id: 2,
-      title: "Clean floor",
-      description: "Clean floor with vaccum",
-      assignee: ["Park", "Mark", "Kate"],
-      assignedDate: "11/12/1111",
-      status: "open",
-      currRoommate: "Park"
-    },
-    {
-      id: 3,
-      title: "Move TV",
-      description: "Move TV away from there",
-      assignee: ["Kate", "Mark", "Park"],
-      assignedDate: "11/19/1111",
-      status: "open",
-      currRoommate: "Kate"
-    }],
+    chores: [
+      {
+        id: 1,
+        title: "Wash Dishses",
+        description: "Wash dishes with hand",
+        assignee: ["Mark", "Park", "Kate"],
+        assignedDate: "11/11/1111",
+        status: "open",
+        currRoommate: "Mark",
+      },
+      {
+        id: 2,
+        title: "Clean floor",
+        description: "Clean floor with vaccum",
+        assignee: ["Park", "Mark", "Kate"],
+        assignedDate: "11/12/1111",
+        status: "open",
+        currRoommate: "Park",
+      },
+      {
+        id: 3,
+        title: "Move TV",
+        description: "Move TV away from there",
+        assignee: ["Kate", "Mark", "Park"],
+        assignedDate: "11/19/1111",
+        status: "open",
+        currRoommate: "Kate",
+      },
+    ],
     archived: [],
     openChoresCount: 3,
     closedChoresCount: 0,
@@ -274,8 +282,8 @@ test("checks readChore returns the correct JSON when multiple non-empty chores a
   functionAPIs.createChore(formData1);
   functionAPIs.createChore(formData2);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   console.log("local storage:", localStorageData);
   console.log("supposed data:", resData);
   expect(localStorageData).toStrictEqual(resData);
@@ -331,7 +339,7 @@ test("check if queryChore returns A JSON with info on a chore when local storage
     assignee: ["Kate", "Mark", "Park"],
     assignedDate: "11/19/1111",
     status: "open",
-    currRoommate: "Kate"
+    currRoommate: "Kate",
   };
 
   const id = 3;
@@ -391,33 +399,35 @@ test("checks if updateChore correctly updates multiple non-empty chores in local
   };
 
   const resData = {
-    chores: [{
-      id: 1,
-      title: "Wash Socks",
-      description: "Wash socks with hand",
-      assignee: ["Pete", "Park", "Kate"],
-      assignedDate: "12/11/1111",
-      status: "open",
-      currRoommate: "Pete"
-    },
-    {
-      id: 2,
-      title: "Clean roof",
-      description: "Clean roof with vaccum",
-      assignee: ["Park", "Pete", "Kate"],
-      assignedDate: "12/12/1111",
-      status: "open",
-      currRoommate: "Park"
-    },
-    {
-      id: 3,
-      title: "Move closet",
-      description: "Move closet away from there",
-      assignee: ["Kitty", "Pete", "Prince"],
-      assignedDate: "01/19/1112",
-      status: "open",
-      currRoommate: "Kitty"
-    }],
+    chores: [
+      {
+        id: 1,
+        title: "Wash Socks",
+        description: "Wash socks with hand",
+        assignee: ["Pete", "Park", "Kate"],
+        assignedDate: "12/11/1111",
+        status: "open",
+        currRoommate: "Pete",
+      },
+      {
+        id: 2,
+        title: "Clean roof",
+        description: "Clean roof with vaccum",
+        assignee: ["Park", "Pete", "Kate"],
+        assignedDate: "12/12/1111",
+        status: "open",
+        currRoommate: "Park",
+      },
+      {
+        id: 3,
+        title: "Move closet",
+        description: "Move closet away from there",
+        assignee: ["Kitty", "Pete", "Prince"],
+        assignedDate: "01/19/1112",
+        status: "open",
+        currRoommate: "Kitty",
+      },
+    ],
     archived: [],
     openChoresCount: 3,
     closedChoresCount: 0,
@@ -431,8 +441,8 @@ test("checks if updateChore correctly updates multiple non-empty chores in local
   functionAPIs.updateChore(2, "chores", upData1);
   functionAPIs.updateChore(3, "chores", upData2);
   const localStorageData = JSON.parse(
-      window.localStorage.getItem("ChoresListData")
-    );
+    window.localStorage.getItem("ChoresListData")
+  );
   console.log("local storage:", localStorageData);
   console.log("supposed data:", resData);
   expect(localStorageData).toStrictEqual(resData);
