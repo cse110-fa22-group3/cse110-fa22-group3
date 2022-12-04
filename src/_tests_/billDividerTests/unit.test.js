8c2061f00f51646125597ed74db505b9a261660d
IzzaTommy <42631988+IzzaTommy@users.noreply.github.com>
2022-12-03 22:30:07 -0800

    Added example test to unit.test.js for billDivider

--- src/_tests_/billDividerTests/unit.test.js
+++ src/_tests_/billDividerTests/unit.test.js
@@ -3,3 +3,10 @@
  */
 
 // import functions
+const functionAPIs = require("../../testSupportFiles/billDividerTests/unit-test-billDividerAPI");
+
+test("what are you testing?", () => {
+    //functionAPIs.getHistoryArray();
+    //expect(...).toStrictEqual(...);
+});
+  
