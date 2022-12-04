ba5121f8fcd41b665a137b0bb8c7d6d4a033783a
Kalyan-Sunkara <kalyant.sunkara@outlook.com>
2022-12-03 23:59:05 -0800

    removing console.logs

---
+++ src/_tests_/passwordHolderTests/frontendTest.js
@@ -0,0 +1,11 @@
+describe("Basic user flow for Website", () => {
+    // First, visit the lab 8 website
+    beforeAll(async () => {
+      await page.goto("http://127.0.0.1:5500/src/front-end/passwords.html");
+    });
+  
+    // Next, check to make sure that all 20 <product-item> elements have loaded
+    it("Initial Passwords Page", async () => {
+      expect(true).toBe(true);
+    });
+  });
