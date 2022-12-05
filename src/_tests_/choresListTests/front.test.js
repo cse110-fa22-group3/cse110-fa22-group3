8fe269ac38e7dc4280e9bfc0486868b226cf131e
raymond50romero <raymond50romero@users.noreply.github.com>
2022-12-05 06:27:44 +0000

    Prettified Code!

--- src/_tests_/choresListTests/front.test.js
+++ src/_tests_/choresListTests/front.test.js
@@ -125,7 +125,7 @@ describe("user flow for chores page", () => {
   // check to see if we can update choress from the list
   it("Check update choress", async () => {
     // click on the chores list
-    await page.click('#\\31');
+    await page.click("#\\31");
     // wait for response
     await page.waitForSelector("#edit-background");
     // change chores text to "changed"
